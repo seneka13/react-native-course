667a8d5e8530c6407dc3fbd67d461f4025fbb97e
seneka13 <62692646+seneka13@users.noreply.github.com>
2021-03-22 17:47:54 +0600

    finish block 2

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { FlatList, ScrollView, StyleSheet, View } from "react-native";
+import AddTodo from "./src/AddTodo";
+import { Navbar } from "./src/NavBar";
+import Todo from "./src/Todo";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const addTodo = (title) => {
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title,
+      },
+    ]);
+  };
+
+  const removeTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Navbar title={"Hello REACT NATIVE"} />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => <Todo todo={item} onRemove={removeTodo} />}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 30,
+    paddingHorizontal: 30,
+    overflow: "scroll",
   },
+
+  text: {},
 });
---
+++ src/Todo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Todo = ({ todo, onRemove }) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={() => console.log(todo.id)}
+      onLongPress={onRemove.bind(null, todo.id)}
+    >
+      <View style={styles.todo}>
+        <Text>{todo.title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  todo: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 15,
+    borderWidth: 1,
+    borderColor: "#eee",
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+});
+
+export default Todo;
